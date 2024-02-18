85bc1ed72f6b97c30cfc1f6ee69da2ffc0892777
Roman Mutel <romanmutel@gmail.com>
2024-02-18 20:35:19 +0200

    done task 4

---
+++ task_4/concurrent_no_locks.py
@@ -0,0 +1,19 @@
+import hazelcast
+
+
+if __name__ == "__main__":
+    print("Launched Client!")
+    KEY = "key"
+    client = hazelcast.HazelcastClient(cluster_name="lab-2", cluster_members=["172.20.0.3:5701"])
+    print("Client connected to cluster")
+    map = client.get_map("dist-map-no-lock").blocking()
+    print("Client got the map and starts incrementing")
+    if not map.contains_key(KEY):
+        map.put(KEY, 0)
+
+    for i in range(10000):
+        if i % 1000 == 1:
+            print(f"Client incremented the value {i} times (pessimistic lock)")
+        map.put(KEY, map.get_entry_view(KEY).value + 1)
+
+    client.shutdown()
---
+++ task_4/concurrent_optimistic.py
@@ -0,0 +1,21 @@
+import hazelcast
+
+
+if __name__ == "__main__":
+    print("Launched Client!")
+    KEY = "key"
+    client = hazelcast.HazelcastClient(cluster_name="lab-2", cluster_members=["172.20.0.3:5701"])
+    print("Client connected to cluster")
+    map = client.get_map("dist-map-no-lock").blocking()
+    print("Client got the map and starts incrementing")
+    map.put_if_absent(KEY, 0)
+
+    i = 0
+    while i < 10000:
+        if i % 1000 == 1:
+            print(f"Client incremented the value {i} times")
+        old_value = map.get_entry_view(KEY).value
+        if map.replace_if_same(KEY, old_value, old_value + 1):
+            i += 1
+
+    client.shutdown()

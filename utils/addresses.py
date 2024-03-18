4b507e557eecf81955ebe8263e33b38358df38bc
Roman Mutel <romanmutel@gmail.com>
2024-03-18 21:06:47 +0200

    renamed mq to kafka

--- utils/addresses.py
+++ utils/addresses.py
@@ -7,5 +7,5 @@ Address: Dict[str, str | List[str]] = {
                 "http://logger-3:8001"
                 ],
     "MESSAGES": "http://messages:8002",
-    "KAFKA": "mq:9092"
+    "KAFKA": "kafka:9092"
 }
